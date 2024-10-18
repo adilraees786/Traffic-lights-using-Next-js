b9ec3d1b8b15f0a8a8ad499e38fa18e93ebe9001
adilraees786 <adilraees911@gmail.com>
2024-10-18 15:31:41 +0500

    footer

--- app/page.js
+++ app/page.js
@@ -47,6 +47,18 @@ function TrafficLight() {
           }`}
         />
       </div>
+      <div>
+      <footer className="bg-gray-800 text-white py-2">
+      <div className="container mx-auto text-center">
+        <p>&copy; {new Date().getFullYear()} My Website. All rights reserved.</p>
+        <div className="flex justify-center space-x-4 mt-2">
+          <a href="#" className="hover:text-gray-400">Privacy Policy</a>
+          <a href="#" className="hover:text-gray-400">Terms of Service</a>
+          <a href="#" className="hover:text-gray-400">Contact</a>
+        </div>
+      </div>
+    </footer>
+      </div>
     </div>
   );
 }
